cd7ca3be9d5836330b3e29394f1bbc6de7f783b3
Richard Haeser <richard@maxserv.com>
2018-04-19 12:03:28 +0200

    initial ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Demo extension',
+    'description' => '',
+    'category' => 'misc',
+    'author' => 'Richard Haeser',
+    'author_email' => '',
+    'clearCacheOnLoad' => 0,
+    'dependencies' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'version' => '2.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    'autoload' => array(
+        'psr-4' => array('Haassie\\DemoExtension\\' => 'Classes')
+    ),
+    'conflicts' => '',
+    'suggests' => array(),
+);
